f06a4de641e5fb7b90623082b9e8812a98b5c2b0
Ritesh dolas <dolasritesh97@gmail.com>
2025-05-23 15:53:03 +0530

    Add controller functions likes, followers, and comments

--- src/controllers/getUserData.controllers.js
+++ src/controllers/getUserData.controllers.js
@@ -1,3 +1,6 @@
+import { Comment } from "../models/comments.models.js";
+import { Follower } from "../models/followers.models.js";
+import { Like } from "../models/likes.models.js";
 import { Post } from "../models/post.models.js";
 import { User } from "../models/user.models.js";
 
@@ -19,8 +22,8 @@ const searchUser = async (req, res) => {
 const getAllUser = async (req, res) => {
   try {
     const user = await User.find({}, { password: 0 });
-
-    if (!user) return res.status(404).json({ message: "user not found" });
+    if (user.length === 0)
+      return res.status(404).json({ message: "no users found" });
 
     return res.status(200).json({ user });
   } catch (error) {
@@ -31,7 +34,6 @@ const getAllUser = async (req, res) => {
 
 const getPostById = async (req, res) => {
   const { postId } = req.params;
-  console.log("postid: ", postId);
 
   if (!postId) return res.status(403).json({ message: "enter post id" });
   try {
@@ -49,8 +51,8 @@ const getPostById = async (req, res) => {
 const getAllPost = async (req, res) => {
   try {
     const post = await Post.find().populate("user_id", "username");
-
-    if (!post) return res.status(404).json({ message: "post not found" });
+    if (post.length === 0)
+      return res.status(404).json({ message: "no post found" });
 
     return res.status(200).json({ post });
   } catch (error) {
@@ -58,4 +60,110 @@ const getAllPost = async (req, res) => {
     return res.status(500).json({ error: "internal server error" });
   }
 };
-export { searchUser, getAllUser, getPostById, getAllPost };
+
+const getAllLikes = async (req, res) => {
+  try {
+    const likes = await Like.find().populate("user_id", "username");
+    if (likes.length === 0)
+      return res.status(404).json({ message: "no likes found" });
+
+    return res.status(200).json({ likes });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "internal server error" });
+  }
+};
+
+const getLikesById = async (req, res) => {
+  const { likeId } = req.params;
+
+  if (!likeId) return res.status(403).json({ message: "enter like id" });
+  try {
+    const likes = await Like.findById(likeId).populate("user_id", "username");
+
+    if (!likes)
+      return res.status(404).json({ message: "liked post not found" });
+
+    return res.status(200).json({ likes });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "internal server error" });
+  }
+};
+const getAllFollower = async (req, res) => {
+  try {
+    const followers = await Follower.find()
+      .populate("follower_id", "username")
+      .populate("followed_id", "username");
+    if (followers.length === 0)
+      return res.status(404).json({ message: "no followers found" });
+
+    return res.status(200).json({ followers });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "internal server error" });
+  }
+};
+
+const getFollowerById = async (req, res) => {
+  const { followerId } = req.params;
+
+  if (!followerId)
+    return res.status(403).json({ message: "enter follower id" });
+  try {
+    const follower = await Follower.findById(followerId)
+      .populate("follower_id", "username")
+      .populate("followed_id", "username");
+
+    if (!follower)
+      return res.status(404).json({ message: "follower not found" });
+
+    return res.status(200).json({ follower });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "internal server error" });
+  }
+};
+const getAllComment = async (req, res) => {
+  try {
+    const comments = await Comment.find().populate("user_id", "username");
+    if (comments.length === 0)
+      return res.status(404).json({ message: "no comment found" });
+
+    return res.status(200).json({ comments });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "internal server error" });
+  }
+};
+
+const getCommentById = async (req, res) => {
+  const { commentId } = req.params;
+
+  if (!commentId) return res.status(403).json({ message: "enter comment id" });
+  try {
+    const comment = await Comment.findById(commentId).populate(
+      "user_id",
+      "username"
+    );
+
+    if (!comment) return res.status(404).json({ message: "comment not found" });
+
+    return res.status(200).json({ comment });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "internal server error" });
+  }
+};
+export {
+  searchUser,
+  getAllUser,
+  getPostById,
+  getAllPost,
+  getLikesById,
+  getAllLikes,
+  getAllFollower,
+  getFollowerById,
+  getAllComment,
+  getCommentById,
+};
