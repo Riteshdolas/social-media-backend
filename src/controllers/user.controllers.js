e4d267cc121521d84918b71827de9f22f5bb84dc
Ritesh dolas <dolasritesh97@gmail.com>
2025-06-29 08:59:27 +0530

    modified user.controllers.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -54,6 +54,7 @@ const registerUser = async (req, res) => {
 
     return res.status(200).json({ savedUser, token });
   } catch (error) {
+    console.log(error)
     return res.status(500).json({ error: "something went wrong" });
   }
 };
